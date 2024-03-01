af56a93b516e0b97714860d99899215b5c39a431
Piece Master <piecemaster720@gmail.com>
2024-03-01 20:17:26 +0545

    add: Dynamic Routs, Handling error, NextAuth, Metadata, Errormessage,

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -7,10 +9,15 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState);
+
   return (
-    <form>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -23,6 +30,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -34,7 +42,18 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <div
+              id="customer-error"
+              aria-live="polite"
+              aria-atomic="true"
+            ></div>
           </div>
+          {state.errors?.customerId &&
+            state.errors.customerId.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
         </div>
 
         {/* Invoice Amount */}
@@ -51,10 +70,19 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
+                // required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <div id="amount-error" aria-live="polite" aria-atomic="true"></div>
           </div>
+          {state.errors?.amount &&
+            state.errors.amount.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
         </div>
 
         {/* Invoice Status */}
@@ -72,6 +100,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
+                <div
+                  id="amount-error"
+                  aria-live="polite"
+                  aria-atomic="true"
+                ></div>
                 <label
                   htmlFor="pending"
                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
@@ -87,6 +120,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
+                <div
+                  id="amount-error"
+                  aria-live="polite"
+                  aria-atomic="true"
+                ></div>
                 <label
                   htmlFor="paid"
                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
@@ -97,6 +135,12 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
         </fieldset>
+        {state.errors?.status &&
+          state.errors.status.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))}
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
@@ -110,3 +154,154 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
     </form>
   );
 }
+
+
+
+
+
+
+
+
+// 'use client';
+// import { useState } from 'react';
+// import { createInvoice } from '@/app/lib/actions';
+// import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroicons/react/24/outline';
+// import Link from 'next/link';
+// import { Button } from '@/app/ui/button';
+// import { CustomerField } from '@/app/lib/definitions';
+
+// export default function Form({ customers }: { customers: CustomerField[] }) {
+//   const [customerError, setCustomerError] = useState<string>('');
+//   const [amountError, setAmountError] = useState<string>('');
+//   const [statusError, setStatusError] = useState<string>('');
+
+//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     let formIsValid = true;
+
+//     if (!event.currentTarget.customerId.value) {
+//       setCustomerError('Please choose a customer');
+//       formIsValid = false;
+//     } else {
+//       setCustomerError('');
+//     }
+
+//     if (!event.currentTarget.amount.value) {
+//       setAmountError('Please enter an amount');
+//       formIsValid = false;
+//     } else {
+//       setAmountError('');
+//     }
+
+//     if (!event.currentTarget.status.value) {
+//       setStatusError('Please set the invoice status');
+//       formIsValid = false;
+//     } else {
+//       setStatusError('');
+//     }
+
+//     if (formIsValid) {
+//       const formData = new FormData(event.currentTarget);
+//       createInvoice(formData);
+//     }
+
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div className="rounded-md bg-gray-50 p-4 md:p-6">
+//         <div className="mb-4">
+//           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
+//             Choose customer
+//           </label>
+//           <div className="relative">
+//             <select
+//               id="customer"
+//               name="customerId"
+//               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+//               defaultValue=""
+//             >
+//               <option value="" disabled>
+//                 Select a customer
+//               </option>
+//               {customers.map((customer) => (
+//                 <option key={customer.id} value={customer.id}>
+//                   {customer.name}
+//                 </option>
+//               ))}
+//             </select>
+//             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+//           </div>
+//             <span className="text-red-500 text-sm">{customerError}</span>
+//         </div>
+
+//         <div className="mb-4">
+//           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+//             Choose an amount
+//           </label>
+//           <div className="relative mt-2 rounded-md">
+//             <div className="relative">
+//               <input
+//                 id="amount"
+//                 name="amount"
+//                 type="number"
+//                 step="0.01"
+//                 placeholder="Enter USD amount"
+//                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+//               />
+//               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+//             </div>
+//               <span className="text-red-500 text-sm">{amountError}</span>
+//           </div>
+//         </div>
+
+//         <fieldset>
+//           <legend className="mb-2 block text-sm font-medium">
+//             Set the invoice status
+//           </legend>
+//           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+//             <div className="flex gap-4">
+//               <div className="flex items-center">
+//                 <input
+//                   id="pending"
+//                   name="status"
+//                   type="radio"
+//                   value="pending"
+//                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+//                 />
+//                 <label
+//                   htmlFor="pending"
+//                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+//                 >
+//                   Pending <ClockIcon className="h-4 w-4" />
+//                 </label>
+//               </div>
+//               <div className="flex items-center">
+//                 <input
+//                   id="paid"
+//                   name="status"
+//                   type="radio"
+//                   value="paid"
+//                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+//                 />
+//                 <label
+//                   htmlFor="paid"
+//                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+//                 >
+//                   Paid <CheckIcon className="h-4 w-4" />
+//                 </label>
+//               </div>
+//             </div>
+//           </div>
+//           <span className="text-red-500 text-sm">{statusError}</span>
+//         </fieldset>
+//       </div>
+//       <div className="mt-6 flex justify-end gap-4">
+//         <Link href="/dashboard/invoices" className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200">
+//           Cancel
+//         </Link>
+//         <Button type="submit">Create Invoice</Button>
+//       </div>
+//     </form>
+//   );
+// }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +18,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -39,7 +43,7 @@ export default function EditInvoiceForm({
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
                 </option>
-              ))}
+              ))}    
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
